798f4b49cc7cd7952bd0aac99892b0a25671554c
Pedro Corrêa <pehccorrea@gmail.com>
2024-06-19 17:45:56 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int segundaGuerraMundial = 1942;
+            string corFavorita = "Azul";
+            float velocidadeF1 = 25.23f;
+            bool segundaGuerraMundiall = true;
+
+
+            Console.WriteLine(segundaGuerraMundial);
+            Console.WriteLine(corFavorita);
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(segundaGuerraMundiall);
+
+            velocidadeF1 = 1358.33f;
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine("Hello, Word!");
+            Console.Read();
+               
+ 
+            
+
+        }
+    }
+}
